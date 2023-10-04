cd74a6405f58bc3072d486ced8871cdd55f69c16
Andrew <lebed202@mail.ru>
2023-10-04 19:32:38 +0300

    M1:task3 syntaxerror

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+items =[
+    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+    {"id": 2, "name": "Куртка кожаная","quantity":3}
+    {"id": 5, "name": "Coca-cola1 литр","quantity":12}
+    {"id": 7, "name": "Картофель фри","quantity":0}
+    {"id": 8, "name": "Кепка","quantity":124}
+]
+
 def home(request):
     text = """ <h1>" learn django"</h1>
           <strong>Автор</strong>: <i>Лебедкин А Д</i>
@@ -18,14 +26,16 @@ def about(request):
                     <b>e-mail: lebed202@mail.ru</b>  """
     return HttpResponse(textabout)
 
-def get_item(request):
-    textitem = items
-    return HttpResponse(textitem)
+def get_item(request, id):   
+    for item in items:
+        if item[id]==id:
+            result = f"""
+            <h2>Имя: {item["name"]} </h2>
+            <p>Количество: {item["quantity"]}</p>
+        """
+        return HttpResponse(result)
+    return HttpResponseNotFound(f'Item with id={id} not found')
 
-items = [
-    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-    {"id": 2, "name": "Куртка кожаная","quantity":3}
-    {"id": 3, "name": "Coca-cola1 литр","quantity":12}
-    {"id": 4, "name": "Картофель фри","quantity":0}
-    {"id": 5, "name": "Кепка","quantity":124}
-]
+
+
+
