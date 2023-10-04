11bd35b2ae4bf28832ce94a8e0b1d69c258c8007
Andrew <lebed202@mail.ru>
2023-10-04 19:38:56 +0300

    M1:task3

--- MainApp/views.py
+++ MainApp/views.py
@@ -5,9 +5,9 @@ from django.http import HttpResponse
 
 items =[
     {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-    {"id": 2, "name": "Куртка кожаная","quantity":3}
-    {"id": 5, "name": "Coca-cola1 литр","quantity":12}
-    {"id": 7, "name": "Картофель фри","quantity":0}
+    {"id": 2, "name": "Куртка кожаная","quantity":3},
+    {"id": 5, "name": "Coca-cola1 литр","quantity":12},
+    {"id": 7, "name": "Картофель фри","quantity":0},
     {"id": 8, "name": "Кепка","quantity":124}
 ]
 
