ea3227692f6b5969f25b2ba222a759971ddb1a61
Andrew <lebed202@mail.ru>
2023-10-15 18:11:45 +0300

    FirstDjango with error for about url

--- MainApp/views.py
+++ MainApp/views.py
@@ -4,21 +4,7 @@ from MainApp.models import Item
 
 # Create your views here.
 
-autor = {
-    "Имя": "Иван",
-    "Отчество": "Петрович",
-    "Фамилия": "Иванов",
-    "Телефон": "89236000102",
-    "email": "vasya@mail.ru"
-}
 
-items =[
-    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-    {"id": 2, "name": "Куртка кожаная","quantity":3},
-    {"id": 5, "name": "Coca-cola1 литр","quantity":12},
-    {"id": 7, "name": "Картофель фри","quantity":0},
-    {"id": 8, "name": "Кепка","quantity":124}
-]
 
 def home(request):
     #text = """ <h1>" learn django"</h1>
@@ -34,27 +20,41 @@ def home(request):
 
 
 def about(request):
+    author = {
+    "Имя": "Иван",
+    "Отчество": "Петрович",
+    "Фамилия": "Иванов",
+    "телефон": "8-923-600-01-02",
+    "email": "vasya@mail.ru"
+
+    }
     result = f""" 
     <header>
         /<a href="/">Home</a> / <a href="/items">Items</a> / <a href="/about">About</a>
     </header>
-    Имя: <b> {autor['Имя']}</b> <br>
-    Отчество: <b>{autor['Отчество']}</b> <br>
-    Фамилия: <b>{autor['Фамилия']}</b> <br>
-    Телефон:<b>{autor['Телефон']}</b><br>
-    email: <b>{autor['email']}</b>  
+    Имя: <b> {author['Имя']}</b> <br>
+    Отчество: <b>{author['Отчество']}</b> <br>
+    Фамилия: <b>{author['Фамилия']}</b> <br>
+    Телефон:<b>{author['Телефон']}</b><br>
+    email: <b>{author['email']}</b>  
     """
     return HttpResponse(result)
 
 def get_item(request, item_id):
         
     try:
-        item = Item.objects.get(id=item_id)        
+        item = Item.objects.get(id=item_id)
+        colors = []
+        # Проверяем, есть ли хоть один цвет у товара
+        if item.colors.exists():
+            colors = item.colors.all()
+     
     except Item.DoesNotExist:      
        return HttpResponseNotFound(f'Item with id={item_id} not found')
     else:
         context = {          
-            "item": item
+            "item": item,
+            "colors": colors
         }
         return render(request, "item-page.html", context)
 
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # FirstDjango
+# 1. `python3 -m venv venv_name` - создание виртуального окружения
+# 2. `source venv_name/bin/activate` - активация ВО
+# 3. `pip install -r requirements.txt` - установка пакетов для ВО
+# 4. `python manage.py makemigrations` - создание миграции
+# 4. `python manage.py migrate`  - с помощью миграций создаем нужные таблицы в БД
+# 4. `python manage.py runserver` - запуск сервера
+
+## Запуск терминала в контексте django
+# `python manage.py shell_plus --ipython(--bpython)`
